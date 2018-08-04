c96a7047c0b624a1e7972572fc665f8bb168bece
maechabin <mitoppo@gmail.com>
2018-08-05 03:20:25 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-vue val="Vue"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloVue from './components/HelloVue.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    HelloVue,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/HelloVue.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Hello {{ value }}</h1>
+    <input type="text" v-bind:value="inputValue" v-on:input="handleInput($event)" />
+    <button v-on:click="handleClick()" v-bind:disabled="isDisabled">button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello-vue',
+  props: ['val'],
+  data() {
+    return {
+      value: this.val,
+      inputValue: '',
+    };
+  },
+  computed: {
+    isDisabled() {
+      return this.inputValue === '';
+    },
+  },
+  methods: {
+    handleInput($event) {
+      this.inputValue = $event.target.value;
+    },
+    handleClick() {
+      if (this.inputValue === '') {
+        return;
+      }
+      this.value = this.inputValue;
+      this.inputValue = '';
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
