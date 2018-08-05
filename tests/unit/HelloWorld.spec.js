fa6771b2117eb2a061c828d7c2e610b3d240e8ec
maechabin <mitoppo@gmail.com>
2018-08-05 22:14:59 +0900

    UT

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,12 +1,110 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from '@vue/test-utils';
+import HelloVue from '@/components/HelloVue.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe('HelloVue.vue', () => {
+  it('props', () => {
+    const val = 'Vue';
+    const wrapper = shallowMount(HelloVue, {
+      propsData: { val },
+    });
+
+    expect(wrapper.props().val).toBe(val);
+    expect(wrapper.text()).toMatch(`Hello ${val}`);
+  });
+
+  it('dom', () => {
+    const wrapper = shallowMount(HelloVue);
+
+    expect(wrapper.contains('h1')).toBeTruthy();
+    expect(wrapper.contains('input')).toBeTruthy();
+    expect(wrapper.contains('button')).toBeTruthy();
+  });
+
+  it('input: v-bind:value', () => {
+    const wrapper = shallowMount(HelloVue);
+    wrapper.setData({ inputValue: 'AAA' });
+    expect(wrapper.find('input').element.value).toBe('AAA');
+  });
+
+  it('button: v-bind:disabled', () => {
+    const wrapper = shallowMount(HelloVue);
+    wrapper.setData({ inputValue: '' });
+    expect(wrapper.find('button').element.disabled).toBeTruthy();
+  });
+
+  describe('event', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = shallowMount(HelloVue);
+    });
+
+    it('should call handleInput', () => {
+      const spy = jest.spyOn(wrapper.vm, 'handleInput');
+      wrapper.find('input').trigger('input');
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should call hancleClick', () => {
+      const spy = jest.spyOn(wrapper.vm, 'handleClick');
+      wrapper.setData({ inputValue: 'AAA' });
+      wrapper.find('button').trigger('click');
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should not call handleClick', () => {
+      const wrapper = shallowMount(HelloVue);
+      const spy = jest.spyOn(wrapper.vm, 'handleClick');
+      wrapper.setData({ inputValue: '' });
+      wrapper.find('button').trigger('click');
+
+      expect(spy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('computed: isDisabled', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = shallowMount(HelloVue);
+    });
+
+    it('sholud be true', () => {
+      wrapper.setData({ inputValue: '' });
+      const disabled = wrapper.vm.isDisabled;
+
+      expect(disabled).toBeTruthy();
+    });
+
+    it('sholud be false', () => {
+      wrapper.setData({ inputValue: 'AAA' });
+      const disabled = wrapper.vm.isDisabled;
+
+      expect(disabled).toBeFalsy();
+    });
+  });
+
+  describe('method', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = shallowMount(HelloVue);
+    });
+
+    it('handleInput', () => {
+      const event = {
+        target: { value: 'AAA' },
+      };
+      wrapper.vm.handleInput(event);
+      expect(wrapper.vm.inputValue).toBe('AAA');
+    });
+
+    it('handleClick', () => {
+      wrapper.setData({ inputValue: 'AAA' });
+      wrapper.vm.handleClick();
+      expect(wrapper.vm.value).toBe('AAA');
+      expect(wrapper.vm.inputValue).toBe('');
+    });
+  });
+});
