f20c86e8a2149813a23da628f428d113f83dc597
maechabin <mitoppo@gmail.com>
2018-08-11 13:36:23 +0900

    コメント追加

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -8,22 +8,36 @@
 
 <script>
 export default {
+  /** コンポーネント名 */
   name: 'hello-vue',
+
+  /** 親コンポーネントから受け取るデータ */
   props: ['val'],
+
+  /** コンポーネントに属するデータ（状態） */
   data() {
     return {
       value: this.val,
       inputValue: '',
     };
   },
+
+  /**
+   * lifycycle hooks
+   * mountedはコンポーネントがマウントされる直前に呼ばれる
+   * */
   mounted() {
     console.log('mounted');
   },
+
+  /** いわゆるGet/Setアクセサ */
   computed: {
     isDisabled() {
       return this.inputValue === '';
     },
   },
+
+  /** コンポーネント内で使用するメソッド */
   methods: {
     handleInput($event) {
       this.inputValue = $event.target.value;
@@ -34,9 +48,13 @@ export default {
       }
       this.value = this.inputValue;
       this.inputValue = '';
+
+      /** カスタムイベントのemit */
       this.$emit('handle-click', this.value);
     },
   },
+
+  /** dataの更新を監視し、変更時に呼ばれるメソッド */
   watch: {
     value(newValue, oldValue) {
       console.log(`watch: ${newValue}, ${oldValue}`);
